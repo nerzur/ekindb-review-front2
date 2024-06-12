6f869d8a429279a824d7e9744e56499518853b23
Gabriel Alberto Pérez Guerra <gabrielpga20@gmail.com>
2024-06-12 14:36:22 -0400

    Updated angular 18 core libs

--- src/app/pages/service/ekin-db-review-api-rest.service.ts
+++ src/app/pages/service/ekin-db-review-api-rest.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DatePipe} from "@angular/common";
 import {CountEntriesByDates} from "../api/countEntriesByDates";
